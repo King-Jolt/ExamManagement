d0782a9a86fc758d74d17ff97cdf80dda0c294e3
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-02-08 02:01:49 +0700

    Signed-off-by: scila1996 <iucuaong_1996@yahoo.com.vn>

--- application/view/admin/template/header.php
+++ application/view/admin/template/header.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" href="/extension/bootstrap/css/bootstrap.css" />
 		<link rel="stylesheet" href="/extension/owner/css/custom.css" />
 		<link rel="stylesheet" href="/extension/jquery-confirm/jquery-confirm.min.css" />
-		<script src="/extension/mathjax/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+		<script type="text/javascript" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML"></script>
 		<script src="/extension/jquery/jquery-1.12.4.js"></script>
 		<script src="/extension/bootstrap/js/bootstrap.js"></script>
 		<script src="/extension/ckeditor4/ckeditor.js"></script>
