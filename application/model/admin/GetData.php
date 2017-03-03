c57d8f1fec4d4b873f8021623bdade3aaae21ba3
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-03-04 02:23:22 +0700

    Signed-off-by: scila1996 <iucuaong_1996@yahoo.com.vn>

--- application/model/admin/GetData.php
+++ application/model/admin/GetData.php
@@ -15,14 +15,14 @@ class GetData
 	);
 	public static function list_Category($user_id, $where_name = '')
 	{
-		$query = 'SELECT list_category.* FROM list_category WHERE list_category.user_id = ? AND list_category.name REGEXP ?';
-		$param = array($user_id, $where_name);
+		$query = 'SELECT list_category.* FROM list_category WHERE list_category.user_id = ? AND list_category.name LIKE ?';
+		$param = array($user_id, '%' . $where_name . '%');
 		return new Sql_QueryCommand($query, $param);
 	}
 	public static function list_Exam($user_id, $category_id, $where_title = '')
 	{
-		$query = 'SELECT list_exam.* FROM list_exam WHERE user_id = ? AND category_id = ? AND title REGEXP ?';
-		$param = array($user_id, $category_id, $where_title);
+		$query = 'SELECT list_exam.* FROM list_exam WHERE user_id = ? AND category_id = ? AND title LIKE ?';
+		$param = array($user_id, $category_id, '%' . $where_title . '%');
 		return new Sql_QueryCommand($query, $param);
 	}
 	public static function list_Question($user_id, $category_id, $exam_id)
