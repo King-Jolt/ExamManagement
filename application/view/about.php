b19eeda39b85ac33e00210b3a71c24b5d16031ef
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-03-04 00:56:02 +0700

    Credits Information !

--- application/view/about.php
+++ application/view/about.php
@@ -5,12 +5,13 @@
 </ul>
 <h4> Components </h4>
 <ul>
-	<li><a href="https://github.com/jquery/jquery"> jQuery JavaScript Library - v1.12 </a></li>
-	<li><a href="https://github.com/twbs/bootstrap"> Twitter Bootstrap 3.6 </a></li>
-	<li><a href="https://github.com/ckeditor/ckeditor-dev"> CKEditor 4.6 </a></li>
-	<li><a href="https://github.com/mathjax/MathJax"> MathJax v2.7.0 </a></li>
-	<li><a href="https://github.com/Eonasdan/bootstrap-datetimepicker"> Bootstrap 3 Datepicker v4 </a></li>
-	<li><a href="https://github.com/craftpip/jquery-confirm"> JQuery Confirm v3 </a></li>
+	<li><a href="https://github.com/jquery/jquery" target="_blank"> jQuery JavaScript Library - v1.12 </a></li>
+	<li><a href="https://github.com/twbs/bootstrap" target="_blank"> Twitter Bootstrap 3.6 </a></li>
+	<li><a href="https://github.com/ckeditor/ckeditor-dev" target="_blank"> CKEditor 4.6 </a></li>
+	<li><a href="https://github.com/mathjax/MathJax" target="_blank"> MathJax v2.7.0 </a></li>
+	<li><a href="https://github.com/Eonasdan/bootstrap-datetimepicker" target="_blank"> Bootstrap 3 Datepicker v4 </a></li>
+	<li><a href="https://github.com/craftpip/jquery-confirm" target="_blank"> JQuery Confirm v3 </a></li>
 </ul>
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <a class="github-button" href="https://github.com/scila1996/ExamManagement" data-style="mega" data-count-href="/scila1996/ExamManagement/stargazers" data-count-api="/repos/scila1996/ExamManagement#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star scila1996/ExamManagement on GitHub"> View on GitHub </a>
+<!-- Credits -->
