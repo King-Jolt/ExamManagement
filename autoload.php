6492cd723da3f1715929429ef7233df198204091
scila1996 <iucuaong_1996@yahoo.com.vn>
2017-04-16 11:59:53 +0700

    Fix AUTOLOAD

--- autoload.php
+++ autoload.php
@@ -8,12 +8,20 @@ set_error_handler(function($severity, $message, $file, $line) {
 
 spl_autoload_register(function($class){
 	$class = str_replace('\\', '/', $class . '.php');
-	if (file_exists($class) or file_exists(
+	if (file_exists($class))
+	{
+		require_once $class;
+	}
+	else if (file_exists($file = (
 		$_SERVER['DOCUMENT_ROOT'] . '/' .
 		strtolower(dirname($class)) . '/' .
 		basename($class)
-	))
+	)))
 	{
-		require_once $class;
+		require_once $file;
+	}
+	else
+	{
+		return false;
 	}
 });
